803c1fee1beabf27687c27b0d4ad2e4a35a70772
Jeremy Rickard <jeremy.rickard@microsoft.com>
2019-06-20 12:48:47 -0600

    Fix lint issues

--- bundle/definition/schema_test.go
+++ bundle/definition/schema_test.go
@@ -305,33 +305,33 @@ func TestConvertValue(t *testing.T) {
 	is.NoError(err)
 	is.Equal(5.5, out.(float64))
 
-	out, err = pd.ConvertValue("nope")
+	_, err = pd.ConvertValue("nope")
 	is.Error(err)
 
 	pd.Type = "array"
-	out, err = pd.ConvertValue("nope")
+	_, err = pd.ConvertValue("nope")
 	is.Error(err)
 
-	out, err = pd.ConvertValue("123")
+	_, err = pd.ConvertValue("123")
 	is.Error(err)
 
-	out, err = pd.ConvertValue("true")
+	_, err = pd.ConvertValue("true")
 	is.Error(err)
 
-	out, err = pd.ConvertValue("123.5")
+	_, err = pd.ConvertValue("123.5")
 	is.Error(err)
 
 	pd.Type = "object"
-	out, err = pd.ConvertValue("nope")
+	_, err = pd.ConvertValue("nope")
 	is.Error(err)
 
-	out, err = pd.ConvertValue("123")
+	_, err = pd.ConvertValue("123")
 	is.Error(err)
 
-	out, err = pd.ConvertValue("true")
+	_, err = pd.ConvertValue("true")
 	is.Error(err)
 
-	out, err = pd.ConvertValue("123.5")
+	_, err = pd.ConvertValue("123.5")
 	is.Error(err)
 
 }
