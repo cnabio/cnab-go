6699a2049c53ba9a54924336e3088ab60704e014
Vaughn Dice <vadice@microsoft.com>
2019-06-21 14:51:47 -0600

    ci(brigade.js): general refresh/update
    * add individual check run handler
    * general re-org
    * add semicolons where expected
    * ensure 2 space indent

--- brigade.js
+++ brigade.js
@@ -1,154 +1,181 @@
-const { events, Job, Group } = require("brigadier")
+const { events, Job } = require("brigadier");
 
-const projectOrg = "deislabs"
-const projectName = "cnab-go"
+const projectOrg = "deislabs";
+const projectName = "cnab-go";
 
-const goImg = "golang:1.11"
-const gopath = "/go"
+const goImg = "golang:1.11";
+const gopath = "/go";
 const localPath = gopath + `/src/github.com/${projectOrg}/${projectName}`;
 
-function build(e, project) {
-    // Create a new job to run Go tests
-    var build = new Job(`${projectName}-build`, goImg);
+// **********************************************
+// Event Handlers
+// **********************************************
 
-    // Set a few environment variables.
-    build.env = {
-        DEST_PATH: localPath,
-        GOPATH: gopath
-    };
+events.on("exec", (e, p) => {
+  return test(e, p).run();
+})
 
-    // Run Go unit tests
-    build.tasks = [
-        // Need to move the source into GOPATH so vendor/ works as desired.
-        `mkdir -p ${localPath}`,
-        `cp -a /src/* ${localPath}`,
-        `cp -a /src/.git ${localPath}`,
-        `cd ${localPath}`,
-        "make bootstrap",
-        "make build",
-        "make test",
-        "make lint",
-    ];
+events.on("check_suite:requested", runSuite);
+events.on("check_suite:rerequested", runSuite);
+events.on("check_run:rerequested", checkRequested);
+events.on("issue_comment:created", handleIssueComment);
+events.on("issue_comment:edited", handleIssueComment);
 
-    return build
+// **********************************************
+// Actions
+// **********************************************
+
+function test(e, project) {
+  var test = new Job("tests", goImg);
+
+  // Add a bit of set up for golang build
+  test.env = {
+    DEST_PATH: localPath,
+    GOPATH: gopath
+  };
+
+  test.tasks = [
+    // Need to move the source into GOPATH so vendor/ works as desired.
+    `mkdir -p ${localPath}`,
+    `cp -a /src/* ${localPath}`,
+    `cp -a /src/.git ${localPath}`,
+    `cd ${localPath}`,
+    "make bootstrap",
+    "make build",
+    "make test",
+    "make lint",
+  ];
+
+  return test;
 }
 
 // Here we can add additional Check Runs, which will run in parallel and
 // report their results independently to GitHub
 function runSuite(e, p) {
-    // For now, this is the one-stop shop running build, lint and test targets
-    runTests(e, p).catch(e => { console.error(e.toString()) });
+  // For now, this is the one-stop shop running build, lint and test targets
+  return runTests(e, p);
 }
 
 // runTests is a Check Run that is ran as part of a Checks Suite
 function runTests(e, p) {
-    console.log("Check requested")
+  console.log("Check requested");
 
-    // Create Notification object (which is just a Job to update GH using the Checks API)
-    var note = new Notification(`tests`, e, p);
-    note.conclusion = "";
-    note.title = "Run Tests";
-    note.summary = "Running the test targets for " + e.revision.commit;
-    note.text = "This test will ensure build, linting and tests all pass."
+  // Create Notification object (which is just a Job to update GH using the Checks API)
+  var note = new Notification(`tests`, e, p);
+  note.conclusion = "";
+  note.title = "Run Tests";
+  note.summary = "Running the test targets for " + e.revision.commit;
+  note.text = "This test will ensure build, linting and tests all pass."
 
-    // Send notification, then run, then send pass/fail notification
-    return notificationWrap(build(e, p), note)
+  // Send notification, then run, then send pass/fail notification
+  return notificationWrap(test(e, p), note);
 }
 
 // handleIssueComment handles an issue_comment event, parsing the comment
 // text and determining whether or not to trigger a corresponding action
 function handleIssueComment(e, p) {
-    if (e.payload) {
-      payload = JSON.parse(e.payload);
+  if (e.payload) {
+    payload = JSON.parse(e.payload);
 
-      // Extract the comment body and trim whitespace
-      comment = payload.body.comment.body.trim();
+    // Extract the comment body and trim whitespace
+    comment = payload.body.comment.body.trim();
 
-      // Here we determine if a comment should provoke an action
-      switch(comment) {
-        case "/brig run":
-          return runTests(e, p);
-          break;
-        default:
-          console.log(`No applicable action found for comment: ${comment}`);
-      }
+    // Here we determine if a comment should provoke an action
+    switch(comment) {
+    case "/brig run":
+      return runTests(e, p);
+    default:
+      console.log(`No applicable action found for comment: ${comment}`);
     }
   }
+}
+
+// checkRequested is the default function invoked on a check_run:* event
+//
+// It determines which check is being requested (from the payload body)
+// and runs this particular check, or else throws an error if the check
+// is not found
+function checkRequested(e, p) {
+  if (e.payload) {
+    payload = JSON.parse(e.payload);
+
+    // Extract the check name
+    name = payload.body.check_run.name;
+
+    // Determine which check to run
+    switch(name) {
+    case "tests":
+      return runTests(e, p);
+    default:
+      throw new Error(`No check found with name: ${name}`);
+    }
+  }
+}
+
+// **********************************************
+// Classes/Helpers
+// **********************************************
 
 // A GitHub Check Suite notification
 class Notification {
-    constructor(name, e, p) {
-        this.proj = p;
-        this.payload = e.payload;
-        this.name = name;
-        this.externalID = e.buildID;
-        this.detailsURL = `https://brigadecore.github.io/kashti/builds/${e.buildID}`;
-        this.title = "running check";
-        this.text = "";
-        this.summary = "";
+  constructor(name, e, p) {
+    this.proj = p;
+    this.payload = e.payload;
+    this.name = name;
+    this.externalID = e.buildID;
+    this.detailsURL = `https://brigadecore.github.io/kashti/builds/${ e.buildID }`;
+    this.title = "running check";
+    this.text = "";
+    this.summary = "";
 
-        // count allows us to send the notification multiple times, with a distinct pod name
-        // each time.
-        this.count = 0;
+    // count allows us to send the notification multiple times, with a distinct pod name
+    // each time.
+    this.count = 0;
 
-        // One of: "success", "failure", "neutral", "cancelled", or "timed_out".
-        this.conclusion = "neutral";
-    }
+    // One of: "success", "failure", "neutral", "cancelled", or "timed_out".
+    this.conclusion = "neutral";
+  }
 
-    // Send a new notification, and return a Promise<result>.
-    run() {
-        this.count++
-        var j = new Job(`${this.name}-${this.count}`, "brigadecore/brigade-github-check-run:v0.1.0");
-        j.imageForcePull = true;
-        j.env = {
-            CHECK_CONCLUSION: this.conclusion,
-            CHECK_NAME: this.name,
-            CHECK_TITLE: this.title,
-            CHECK_PAYLOAD: this.payload,
-            CHECK_SUMMARY: this.summary,
-            CHECK_TEXT: this.text,
-            CHECK_DETAILS_URL: this.detailsURL,
-            CHECK_EXTERNAL_ID: this.externalID
-        }
-        return j.run();
-    }
+  // Send a new notification, and return a Promise<result>.
+  run() {
+    this.count++;
+    var job = new Job(`${ this.name }-notification-${ this.count }`, "brigadecore/brigade-github-check-run:v0.1.0");
+    job.imageForcePull = true;
+    job.env = {
+      "CHECK_CONCLUSION": this.conclusion,
+      "CHECK_NAME": this.name,
+      "CHECK_TITLE": this.title,
+      "CHECK_PAYLOAD": this.payload,
+      "CHECK_SUMMARY": this.summary,
+      "CHECK_TEXT": this.text,
+      "CHECK_DETAILS_URL": this.detailsURL,
+      "CHECK_EXTERNAL_ID": this.externalID
+    };
+    return job.run();
+  }
 }
 
 // Helper to wrap a job execution between two notifications.
-async function notificationWrap(job, note, conclusion) {
-    if (conclusion == null) {
-        conclusion = "success"
-    }
-    await note.run();
+async function notificationWrap(job, note) {
+  await note.run();
+  try {
+    let res = await job.run();
+    const logs = await job.logs();
+    note.conclusion = "success";
+    note.summary = `Task "${ job.name }" passed`;
+    note.text = "```" + res.toString() + "```\nTest Complete";
+    return await note.run();
+  } catch (e) {
+    const logs = await job.logs();
+    note.conclusion = "failure";
+    note.summary = `Task "${ job.name }" failed for ${ e.buildID }`;
+    note.text = "```" + logs + "```\nFailed with error: " + e.toString();
     try {
-        let res = await job.run()
-        const logs = await job.logs();
-
-        note.conclusion = conclusion;
-        note.summary = `Task "${job.name}" passed`;
-        note.text = note.text = "```" + res.toString() + "```\nTest Complete";
-        return await note.run();
-    } catch (e) {
-        const logs = await job.logs();
-        note.conclusion = "failure";
-        note.summary = `Task "${job.name}" failed for ${e.buildID}`;
-        note.text = "```" + logs + "```\nFailed with error: " + e.toString();
-        try {
-            return await note.run();
-        } catch (e2) {
-            console.error("failed to send notification: " + e2.toString());
-            console.error("original error: " + e.toString());
-            return e2;
-        }
+      await note.run();
+    } catch (e2) {
+      console.error("failed to send notification: " + e2.toString());
+      console.error("original error: " + e.toString());
     }
+    throw e;
+  }
 }
-
-events.on("exec", (e, p) => {
-    return build(e, p).run()
-})
-
-events.on("check_suite:requested", runSuite)
-events.on("check_suite:rerequested", runSuite)
-events.on("check_run:rerequested", runSuite)
-events.on("issue_comment:created", handleIssueComment);
-events.on("issue_comment:edited", handleIssueComment);
