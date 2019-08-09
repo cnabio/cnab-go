c992a169ba13fb2f36857dbddfb45e6790400758
Vaughn Dice <vadice@microsoft.com>
2019-08-09 10:17:47 -0600

    ref(bundle/*): replace mapstructure tags with yaml tags (#95)

--- bundle/outputs.go
+++ bundle/outputs.go
@@ -1,8 +1,8 @@
 package bundle
 
 type Output struct {
-	Definition  string   `json:"definition" mapstructure:"definition"`
-	ApplyTo     []string `json:"applyTo,omitempty" mapstructure:"applyTo,omitempty"`
-	Description string   `json:"description,omitempty" mapstructure:"description"`
-	Path        string   `json:"path" mapstructure:"path"`
+	Definition  string   `json:"definition" yaml:"definition"`
+	ApplyTo     []string `json:"applyTo,omitempty" yaml:"applyTo,omitempty"`
+	Description string   `json:"description,omitempty" yaml:"description,omitempty"`
+	Path        string   `json:"path" yaml:"path"`
 }
