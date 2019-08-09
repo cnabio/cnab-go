d6372591dfbf1e85f019e4521cd49fb281fc2397
Radu M <root@radu.sh>
2019-08-09 17:37:00 +0300

    Remove fields from outputs
    Signed-off-by: Radu M <root@radu.sh>

--- bundle/outputs.go
+++ bundle/outputs.go
@@ -1,10 +1,6 @@
 package bundle
 
-type OutputsDefinition struct {
-	Fields map[string]OutputDefinition `json:"fields" mapstructure:"fields"`
-}
-
-type OutputDefinition struct {
+type Output struct {
 	Definition  string   `json:"definition" mapstructure:"definition"`
 	ApplyTo     []string `json:"applyTo,omitempty" mapstructure:"applyTo,omitempty"`
 	Description string   `json:"description,omitempty" mapstructure:"description"`
