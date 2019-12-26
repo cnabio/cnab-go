0238ebbdb1b81b9c46216270cd5c1b3b112be917
weidong.fw <weidong.fw@alibaba-inc.com>
2019-12-26 10:50:02 +0800

    fix(driver/docker): wait container to stop after container started, not before
    Signed-off-by: weidong.fw <weidong.fw@alibaba-inc.com>

--- driver/docker/docker.go
+++ driver/docker/docker.go
@@ -231,10 +231,10 @@ func (d *Driver) exec(op *driver.Operation) (driver.OperationResult, error) {
 		}
 	}()
 
-	statusc, errc := cli.Client().ContainerWait(ctx, resp.ID, container.WaitConditionNextExit)
 	if err = cli.Client().ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
 		return driver.OperationResult{}, fmt.Errorf("cannot start container: %v", err)
 	}
+	statusc, errc := cli.Client().ContainerWait(ctx, resp.ID, container.WaitConditionNextExit)
 	select {
 	case err := <-errc:
 		if err != nil {
