03aaa7d41c1c674a6d1a15ad6acff12ecdd85127
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2020-04-07 14:45:27 -0500

    Export crud.MockStore and allow mocking errors
    The MockStore struct is very useful for tests outside of the crud
    package, since it has an in-memory implementation of the crud.Store
    interface. This adds the ability to mock errors to test out how code
    that wrap the store handle edge cases and then exports the struct.
    
    Signed-off-by: Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>

--- utils/crud/backingstore_test.go
+++ utils/crud/backingstore_test.go
@@ -19,11 +19,11 @@ func TestBackingStore_Read(t *testing.T) {
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
 			s := NewMockStore()
-			s.data[TestItemType] = map[string][]byte{"key1": []byte("value1")}
+			s.data[testItemType] = map[string][]byte{"key1": []byte("value1")}
 			bs := NewBackingStore(s)
 			bs.AutoClose = tc.autoclose
 
-			val, err := bs.Read(TestItemType, "key1")
+			val, err := bs.Read(testItemType, "key1")
 			require.NoError(t, err, "expected Read to succeed")
 			assert.Equal(t, "value1", string(val), "Read returned the wrong data")
 
@@ -57,7 +57,7 @@ func TestBackingStore_Store(t *testing.T) {
 			bs := NewBackingStore(s)
 			bs.AutoClose = tc.autoclose
 
-			err := bs.Save(TestItemType, "key1", []byte("value1"))
+			err := bs.Save(testItemType, "key1", []byte("value1"))
 			require.NoError(t, err, "expected Store to succeed")
 
 			connectCount, err := s.GetConnectCount()
@@ -72,7 +72,7 @@ func TestBackingStore_Store(t *testing.T) {
 				assert.Equal(t, 0, closeCount, "Close should not be automatically called")
 			}
 
-			val, err := bs.Read(TestItemType, "key1")
+			val, err := bs.Read(testItemType, "key1")
 			require.NoError(t, err, "expected Read to succeed")
 			assert.Equal(t, "value1", string(val), "stored value did not survive the round trip")
 
@@ -107,14 +107,14 @@ func TestBackingStore_List(t *testing.T) {
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
 			s := NewMockStore()
-			s.data[TestItemType] = map[string][]byte{
+			s.data[testItemType] = map[string][]byte{
 				"key1": []byte("value1"),
 				"key2": []byte("value2"),
 			}
 			bs := NewBackingStore(s)
 			bs.AutoClose = tc.autoclose
 
-			results, err := bs.List(TestItemType)
+			results, err := bs.List(testItemType)
 			require.NoError(t, err, "expected List to succeed")
 			require.Contains(t, results, "key1")
 			require.Contains(t, results, "key2")
@@ -146,11 +146,11 @@ func TestBackingStore_Delete(t *testing.T) {
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
 			s := NewMockStore()
-			s.data[TestItemType] = map[string][]byte{"key1": []byte("value1")}
+			s.data[testItemType] = map[string][]byte{"key1": []byte("value1")}
 			bs := NewBackingStore(s)
 			bs.AutoClose = tc.autoclose
 
-			err := bs.Delete(TestItemType, "key1")
+			err := bs.Delete(testItemType, "key1")
 			require.NoError(t, err, "expected Delete to succeed")
 
 			connectCount, err := s.GetConnectCount()
@@ -165,7 +165,7 @@ func TestBackingStore_Delete(t *testing.T) {
 				assert.Equal(t, 0, closeCount, "Close should not be automatically called")
 			}
 
-			val, _ := bs.Read(TestItemType, "key1")
+			val, _ := bs.Read(testItemType, "key1")
 			assert.Empty(t, val, "Delete should have removed the entry")
 		})
 	}
@@ -183,14 +183,14 @@ func TestBackingStore_ReadAll(t *testing.T) {
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
 			s := NewMockStore()
-			s.data[TestItemType] = map[string][]byte{
+			s.data[testItemType] = map[string][]byte{
 				"key1": []byte("value1"),
 				"key2": []byte("value2"),
 			}
 			bs := NewBackingStore(s)
 			bs.AutoClose = tc.autoclose
 
-			results, err := bs.ReadAll(TestItemType)
+			results, err := bs.ReadAll(testItemType)
 			require.NoError(t, err, "expected ReadAll to succeed")
 			assert.Contains(t, results, []byte("value1"))
 			assert.Contains(t, results, []byte("value2"))
