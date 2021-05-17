e36a6a36b7d68b2a1c7bb80323bf0bdb16152f8c
Vaughn Dice <vadice@microsoft.com>
2021-05-17 14:21:11 -0600

    fix linter err
    Signed-off-by: Vaughn Dice <vadice@microsoft.com>

--- bundle/parameters_test.go
+++ bundle/parameters_test.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/cnabio/cnab-go/bundle/definition"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/cnabio/cnab-go/bundle/definition"
 )
 
 func TestCanReadParameterNames(t *testing.T) {
