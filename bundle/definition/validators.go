4d05bf9fac74a5605c6d1af72150824697780241
Yury Rudichev <rudichev@kaspersky.com>
2024-10-13 09:37:21 +0300

    remove commented code
    Signed-off-by: Yury Rudichev <rudichev@kaspersky.com>

--- bundle/definition/validators.go
+++ bundle/definition/validators.go
@@ -4,40 +4,8 @@ import (
 	jsonschema "github.com/santhosh-tekuri/jsonschema/v6"
 )
 
-// ContentEncoding represents a "custom" Schema property
-type ContentEncoding string
-
-// // NewContentEncoding allocates a new ContentEncoding validator
-// func NewContentEncoding() jsonschema.Keyword {
-// 	return new(ContentEncoding)
-// }
-
-// func (c ContentEncoding) Validate(propPath string, data interface{}, errs *[]jsonschema.KeyError) {}
-
-// func (c ContentEncoding) ValidateKeyword(ctx context.Context, currentState *jsonschema.ValidationState, data interface{}) {
-// 	if obj, ok := data.(string); ok {
-// 		switch c {
-// 		case "base64":
-// 			_, err := base64.StdEncoding.DecodeString(obj)
-// 			if err != nil {
-// 				currentState.AddError(data, fmt.Sprintf("invalid %s value: %s", c, obj))
-// 			}
-// 		// Add validation support for other encodings as needed
-// 		// See https://json-schema.org/latest/json-schema-validation.html#rfc.section.8.3
-// 		default:
-// 			currentState.AddError(data, fmt.Sprintf("unsupported or invalid contentEncoding type of %s", c))
-// 		}
-// 	}
-// }
-
-// func (c ContentEncoding) Register(uri string, registry *jsonschema.SchemaRegistry) {}
-
-// func (c ContentEncoding) Resolve(pointer jsonpointer.Pointer, uri string) *jsonschema.Schema {
-// 	return nil
-// }
-
-// NewRootSchema returns a jsonschema.RootSchema with any needed custom
-// jsonschema.Validators pre-registered
+// NewCompiler returns a jsonschema.Compiler configured for fully support
+// https://json-schema.org/draft/2019-09/schema
 func NewCompiler() *jsonschema.Compiler {
 	c := jsonschema.NewCompiler()
 	c.DefaultDraft(jsonschema.Draft2019)
@@ -45,11 +13,4 @@ func NewCompiler() *jsonschema.Compiler {
 	c.AssertFormat()
 	c.AssertContent()
 	return c
-	// Register custom validators here
-	// Note: as of writing, jsonschema doesn't have a stock validator for instances of type `contentEncoding`
-	// There may be others missing in the library that exist in http://json-schema.org/draft-07/schema#
-	// and thus, we'd need to create/register them here (if not included upstream)
-	// jsonschema.RegisterKeyword("contentEncoding", NewContentEncoding)
-	// jsonschema.LoadDraft2019_09()
-	// return &jsonschema.Schema{}
 }
